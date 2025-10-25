7c93ed2cb59dd792a6b056ecc1397c05aaa4e88c
kuzRK <sanalsogdanov11188.com>
2025-10-25 10:28:50 +0000

    classwork1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 
 int main()
-{}
+{
+  using u_t = unsigned;
+  u_t a = 0;
+  size_t count = 0;
+  while (std::cin >> a) {
+    count++;
+  }
+
+  if (std::cin.eof()) {
+    std::cout << count;
+    std::cout << "\n";
+  } else if (std::cin.fail()) {
+    std::cerr << "input err\n";
+    return 1;
+  }
+
+}
