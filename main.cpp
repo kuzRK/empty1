f7006b023a75d07c6480ee878678a18cbbb46fca
kuzRK <sanalsogdanov11188.com>
2025-10-25 12:34:55 +0000

    overfunc

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
+#include <limits>
 
-bool isPyth( unsigned a, unsigned b, unsigned c)
+unsigned max_u()
+{
+  using u_t = unsigned;
+  using u_limits = std::numeric_limits<u_t>;
+  return u_limits::max();
+}
+
+bool oversquare(unsigned a)
+{
+  unsigned res = 0;
+  if (a > max_u()/a) {
+    return true;
+  }
+  res = a * a;
+  return false;
+}
+
+bool overplus(unsigned a, unsigned b)
+{
+  unsigned res = 0;
+  if (a > max_u() - b) {
+    return true;
+  }
+  res = a + b;
+  return false;
+}
+
+bool isPyth(unsigned a, unsigned b, unsigned c)
 {
   bool p = (b*b + c*c == a*a);
   p  = p || (a*a + c*c == b*b);
   p  = p || (b*b + a*a == c*c);
   return   a;
 }
+
 int main()
 {
   using u_t = unsigned;
@@ -20,7 +49,6 @@ int main()
     c = b;
     b = a;
   }
-
   if (std::cin.eof()) {
     std::cout << count;
     std::cout << "\n";
@@ -28,5 +56,4 @@ int main()
     std::cerr << "input err\n";
     return 1;
   }
-
 }
